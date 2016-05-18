ae0d5b1ac1d982b5f3b87cd77547f75e0bb9e709
DSLewis22 <dl01852@georgiasouthern.edu>
2016-05-18 16:44:40 -0400

    coaches insert

--- CoachData.sql
+++ CoachData.sql
@@ -1,16 +1,30 @@
-use NBA;
-INSERT INTO DBO.COACH(CoachFName,CoachLName)
-    VALUES ('Brad', 'Stevens')
-
-
-
-
-
-
-
-
-
-
-
-
-
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Mike', 'Budenholzer');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Brad', 'Stevens');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Tony', 'Brown');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Steve', 'Clifford');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Fred', 'Hoiberg');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Tyronn', 'Lue');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Rick', 'Carlisle');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Michael', 'Malone');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Stan', 'Van');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Steve', 'Kerr');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('J.B.', 'Bickerstaff');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Vacant', '');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Doc', 'Rivers');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Vacant', '');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Vacant', '');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Erik', 'Spoelstra');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Jason', 'Kidd');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Tom', 'Thibodeau');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Alvin', 'Gentry');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Kurt', 'Rambis');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Billy', 'Donovan');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Scott', 'Skiles');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Brett', 'Brown');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Earl', 'Watson');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Terry', 'Stotts');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('David', 'Joerger');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Gregg', 'Popovich');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Dwane', 'Casey');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Quin', 'Snyder');
+INSERT INTO NBA.dbo.COACH (CoachFName, CoachLName) VALUES ('Scott', 'Brooks');
